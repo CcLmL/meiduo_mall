9cf44aa81a3abe8788a791f916bc97528ebf61a4
xwp <xwp_fullstack@163.com>
2018-09-19 19:25:29 +0800

    用户部分-注册用户信息的保存(jwt扩展类的登录视图obtain_jwt_token)

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token
+
 from users import views
 
 urlpatterns = [
     url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
-    url(r'^users/$', views.UserVIew.as_view())
+    url(r'^users/$', views.UserVIew.as_view()),
+    # Django REST framework JWT提供了登录签发JWT的视图，可以直接使用
+    url(r'^authorizations/$', obtain_jwt_token),  # obtain_jwt_token = ObtainJSONWebToken.as_view()
 ]
