19f8efd5d23b558e4e13a1802afeda7321a8feae
xwp <xwp_fullstack@163.com>
2018-09-21 15:49:14 +0800

    个人中心-获取用户的个人信息(User模型类添加邮箱验证字段)

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -8,8 +8,10 @@ class User(AbstractUser):
     """用户模型类"""
     # 因为Django框架自带用户认证类,所以我们只需要继承对应类并添加自己的设置就可以了
     mobile = models.CharField(max_length=11, verbose_name='手机号')
+    email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
     # openid = models.CharField(max_length=64, verbose_name='OpenID')  # 因为一个用户可以对应多个QQ账户,所以在这里设置openid并不能实现一对多
     # 所以要建立一个单独的表
+
     class Meta:
         db_table = 'tb_users'
         verbose_name = '用户'
