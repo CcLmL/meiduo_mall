bd0610feb30bc9d95884fb7d1019036d351b0f70
xwp <xwp_fullstack@163.com>
2018-09-22 21:08:02 +0800

    省市县三级联动-获取省级地区的信息(后端API)

---
+++ meiduo_mall/meiduo_mall/apps/areas/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from areas import views
+
+urlpatterns = [
+    url(r'^/areas/$', views.AreasView.as_view())
+]
--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from areas.models import Area
+from areas.serializer import AreaSerializer
 
 # Create your views here.
+
+
+# GET /areas/
+# class AreasView(GenericAPIView):
+class AreasView(ListAPIView):
+    serializer_class = AreaSerializer
+    queryset = Area.objects.filter(parent=None)
+
+    # def get(self, request):
+    #     """
+    #     获取所有省级地区的信息
+    #     1. 查询所有省级地区
+    #     2. 将省级地区的信息序列化返回
+    #     """
+    #     # 1. 查询所有省级地区
+    #     areas = self.get_queryset()
+    #
+    #     # 2. 将省级地区的信息序列化返回
+    #     serializer = self.get_serializer(areas, many=True)
+    #
+    #     return Response(serializer.data)
