d3d6be615b942488d684de6141b1c65b37d3c7c8
xwp <xwp_fullstack@163.com>
2018-09-22 21:35:51 +0800

    省市县三级联动-获取指定地区的信息(后端API)

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from areas.models import Area
-from areas.serializer import AreaSerializer
+from areas.serializer import AreaSerializer, SubAreaSerializer
+
 
 # Create your views here.
 
@@ -25,5 +26,27 @@ class AreasView(ListAPIView):
     #
     #     # 2. 将省级地区的信息序列化返回
     #     serializer = self.get_serializer(areas, many=True)
+    #
+    #     return Response(serializer.data)
+
+
+# GET /areas/(?p<pk>\d+)/
+# class SubAreasView(GenericAPIView):
+class SubAreasView(RetrieveAPIView):
+    serializer_class = SubAreaSerializer
+    queryset = Area.objects.all()
+
+    # def get(self, request, pk):
+    #     """
+    #     获取指定地区的信息
+    #     1. 根据pk获取的指定的地区信息
+    #     2. 将地区序列化并返回(注:将地址关联的下级地区进行嵌套序列化)
+    #     """
+    #     # 1. 根据pk获取的指定的地区信息
+    #     # area = Area.objects.get(pk=pk)
+    #     area = self.get_object()  # 这个方法默认就是通过主键查出对象
+    #
+    #     # 2. 将地区序列化并返回(注:将地址关联的下级地区进行嵌套序列化)
+    #     serializer = self.get_serializer(area)
     #
     #     return Response(serializer.data)
