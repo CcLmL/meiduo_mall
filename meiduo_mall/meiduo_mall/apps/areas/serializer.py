2cfdb2be073ee1480c6f0d258c7dec89c2f6a188
xwp <xwp_fullstack@163.com>
2018-09-22 21:36:10 +0800

    省市县三级联动-获取指定地区的信息(后端API)

--- meiduo_mall/meiduo_mall/apps/areas/serializer.py
+++ meiduo_mall/meiduo_mall/apps/areas/serializer.py
@@ -8,3 +8,13 @@ class AreaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Area
         fields = ('id', 'name')
+
+
+class SubAreaSerializer(serializers.ModelSerializer):
+    """地区序列化器类"""
+    # 使用指定的序列化器进行序列化
+    subs = AreaSerializer(label='下级地区', many=True)
+
+    class Meta:
+        model = Area
+        fields = ('id', 'name', 'subs')
